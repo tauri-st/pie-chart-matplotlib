8ad2caa8e2e98e490ff87d7df95e042fbe9ccbbb
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-08 22:17:55 +0000

    Create png with default colors then add variable to assign new colors

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ snack_scores = [40, 30, 30]
 
 slice_labels = ["fries", "chips", "sour candy"]
 
+colors = ["#5F9EA0", "#DDA0DD", "#4682B4"]
+
 # Let's make a pie chart!
 plt.pie(snack_scores, labels=slice_labels)
 
--- replit.nix
+++ replit.nix
@@ -1,3 +1,14 @@
 { pkgs }: {
-  deps = [ ];
+  deps = [
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+   ];
 }
